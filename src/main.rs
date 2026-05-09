0579c04f811eb369f1d31a0bb0467c371a122a91
NullDev <2234223561@qq.com>
2026-05-09 18:03:21 +0800

    Initial commit: project scaffold for campus network auto-auth

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Pulse - Campus Network Auto Auth");
+    println!("Starting heartbeat service...");
+
+    // TODO: 读取配置
+    // TODO: 检测网络连通性
+    // TODO: 发送认证请求
+    // TODO: 定期心跳保活
+
+    Ok(())
+}
